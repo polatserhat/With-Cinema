6ab8bd19ba38dd3b76a7470e71cc94d482fbe164
Serhat Polat <polattserhat@hotmail.com>
2024-03-13 09:56:40 +0300

    Initial Commit

---
+++ WithCinema/WithCinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithCinemaApp.swift
+//  WithCinema
+//
+//  Created by Serhat  on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct WithCinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
